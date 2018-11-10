2e79371d5deb896e483c85fd96188311655436f1
Vincenzo <Vincenzo@DESKTOP-M7I5RHN>
2018-11-10 13:01:09 +0000

    Fixed build

--- easymanager/easymanager-fatturapa/src/test/java/progettotlp/fatturapa/FatturaPaConverterTest.java
+++ easymanager/easymanager-fatturapa/src/test/java/progettotlp/fatturapa/FatturaPaConverterTest.java
@@ -2,18 +2,25 @@ package progettotlp.fatturapa;
 
 import static org.junit.Assert.*;
 
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Properties;
 
 import static org.mockito.Mockito.*;
 import org.junit.Test;
 
+import progettotlp.facilities.ConfigurationManager;
 import progettotlp.interfaces.FatturaInterface;
 
 public class FatturaPaConverterTest {
 
 	@Test
 	public void testCreateProgressivoInvio() throws Exception {
+		InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("easymanager.properties");
+		Properties properties = new Properties();
+		properties.load(resourceAsStream);
+		ConfigurationManager.setProperties(properties);
 		FatturaInterface mockFattura = mock(FatturaInterface.class);
 		Date dateToReturn = new SimpleDateFormat("yyyyMMdd").parse("20180101");
 		when(mockFattura.getEmissione()).thenReturn(dateToReturn);
